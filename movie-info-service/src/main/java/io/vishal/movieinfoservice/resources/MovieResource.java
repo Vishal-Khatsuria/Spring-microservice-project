9b930e719bb6edd9419e80d6bfedfa02e72dc774
Vishal <vishal.khatsuria@gmail.com>
2020-04-04 18:16:27 +0530

    03. Added Movie Info API

---
+++ movie-info-service/src/main/java/io/vishal/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,19 @@
+package io.vishal.movieinfoservice.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.vishal.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	@GetMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+		return new Movie(movieId, "movie Name");
+	}
+
+}
