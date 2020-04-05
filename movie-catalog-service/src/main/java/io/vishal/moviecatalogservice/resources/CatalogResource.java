8b151cb4e1f01123b94eb2f5ca501470f043fd40
Vishal <vishal.khatsuria@gmail.com>
2020-04-05 15:23:57 +0530

    13. Used service discovery to call APIs

--- movie-catalog-service/src/main/java/io/vishal/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/vishal/moviecatalogservice/resources/CatalogResource.java
@@ -25,11 +25,11 @@ public class CatalogResource {
 	@GetMapping("/{userId}")
 	public UserCatalog getcatalog(@PathVariable("userId") String userId){
 		
-		UserRating ratings = restTemplate.getForObject("http://localhost:8087/ratings/users/"+userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratings/users/"+userId, UserRating.class);
 		
 		List<CatalogItem> catalogItem = ratings.getUserRatings().stream()
 				.map(rating -> {
-					Movie movie = restTemplate.getForObject("http://localhost:8086/movies/"+rating.getMovieId(), Movie.class);
+					Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 					return new CatalogItem(movie.getName(), movie.getDesc(), rating.getRating());
 				})
 				.collect(Collectors.toList());
